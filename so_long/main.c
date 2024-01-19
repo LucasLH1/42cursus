36d86267869a173edb33e2090f80f964b659c5c5
LucasLH1 <lahayelucas5@gmail.com>
2024-01-19 12:12:21 +0100

    so_long

--- so_long/main.c
+++ so_long/main.c
@@ -1,26 +1,49 @@
 #include "MacroLibX/includes/mlx.h"
 
+int window_hook(int event, void* mlx)
+{
+    if(event == 0) // 0 is when we trigger the close of the window (by clicking the cross for example)
+        mlx_loop_end(mlx);
+    return (0);
+}
+
+void set_floor(void *mlx, void *win, void *img, int max_x, int max_y)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+
+	while (y < max_y)
+	{
+		while(x < max_x)
+		{
+			mlx_put_image_to_window(mlx, win, img, x, y); // displays the image at x,y = 42,42
+			x+=16;
+		}
+		x = 0;
+		y+=16;
+	}
+}
+
 int main(void)
 {
-    void* mlx = mlx_init();
-    void* win = mlx_new_window(mlx, 400, 400, "Hello world!");
+	void* mlx = mlx_init();
+	void* win = mlx_new_window(mlx, 896, 640, "so_long");
 
-    mlx_set_font(mlx, win, "/path/to/my/awesome/font.ttf");
-    // Now all texts will be rendered using font.ttf
-    mlx_string_put(mlx, win, 200, 210, 0xFFFFFFFF, "this is my text");
-    mlx_string_put(mlx, win, 100, 10, 0xFFFE5FFF, "MacroLibX > all");
-    mlx_string_put(mlx, win, 300, 50, 0xFFFF00FF, "42angouleme");
+	int img_width;
+	int img_height;
+	void* img = mlx_png_file_to_image(mlx, "assets/sprites/tilesets/grass.png", &img_width, &img_height);
 
-    mlx_set_font(mlx, win, "/path/to/my/awesome/another_font.ttf");
-    // Now all texts will be rendered using another_font.ttf
-    mlx_string_put(mlx, win, 0, 20, 0xFFFFFFFF, "Akel");
-    mlx_string_put(mlx, win, 300, 210, 0xFFFE5FFF, "vim > vscode");
-    mlx_string_put(mlx, win, 200, 310, 0xFFFF00FF, "vvaas est mauvais");
+	set_floor(mlx, win, img, 896, 640);
 
-    mlx_set_font_scale(mlx, win, "/path/to/my/awesome/font.ttf", 16.0f);
-    // Now all texts will be rendered using font.ttf but scaled at 16 pixels height
-    mlx_string_put(mlx, win, 200, 210, 0xFFFFFFFF, "kroussar > kiroussa");
-    mlx_string_put(mlx, win, 100, 10, 0xFFFE5FFF, "i use arch btw");
+	mlx_on_event(mlx, win, MLX_WINDOW_EVENT, window_hook, mlx);
 
-    /* loop, cleanup, ... */
+	mlx_loop(mlx);
+
+	// do not forget to destroy the image, otherwise the MacroLibX will yell at you !
+	mlx_destroy_image(mlx, img);
+	mlx_destroy_window(mlx, win);
+	mlx_destroy_display(mlx);
 }
